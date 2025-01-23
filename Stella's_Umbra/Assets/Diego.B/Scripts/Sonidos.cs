d65b74b5cd0bc1ed5653e3d565cc517e862c6a03
DESKTOP-720KFDS\Prueba <borregaalvarezdiego.animacion@gmail.com>
2025-01-24 00:37:25 +0100

    Commit  Añadir efectos de sonido

---
+++ Stella's_Umbra/Assets/Diego.B/Scripts/Sonidos.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class Sonidos : MonoBehaviour
+{
+    private AudioSource audioSource;
+    [SerializeField] AudioClip[] audioClips; // Agrega aquí los clips de audio desde el inspector.
+    private bool isJumping = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (!audioSource.isPlaying)
+            {
+                PlaySpecificClip(0);
+            }
+            else if (Input.GetKey(KeyCode.LeftShift))
+            {
+                PlaySpecificClip(3);
+            }
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            if (!audioSource.isPlaying)
+            {
+                PlaySpecificClip(1);
+                isJumping = true;
+            }
+        }
+        else
+        {
+            // Detener el sonido si no se presiona ninguna tecla relevante
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+    private void PlaySpecificClip(int index)
+    {
+        if (index >= 0 && index < audioClips.Length)
+        {
+            audioSource.clip = audioClips[index];
+            audioSource.Play();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Verificar si el personaje choca contra el suelo
+        if (collision.gameObject.CompareTag("Floor") && isJumping)
+        {
+            PlaySpecificClip(2);
+            isJumping = false; // Resetear estado de salto
+        }
+    }
+
+}
+
