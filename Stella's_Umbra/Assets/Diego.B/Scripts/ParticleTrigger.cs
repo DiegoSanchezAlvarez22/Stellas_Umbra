92c8cced5cc0e4ff701c1324c5c6344d6f85fef0
DESKTOP-720KFDS\Prueba <borregaalvarezdiego.animacion@gmail.com>
2025-02-11 01:42:15 +0100

    Efectos visuales puerta jefe y teleports (Diego B)

--- Stella's_Umbra/Assets/Diego.B/Scripts/ParticleTrigger.cs
+++ Stella's_Umbra/Assets/Diego.B/Scripts/ParticleTrigger.cs
@@ -4,11 +4,27 @@ public class ParticleTrigger : MonoBehaviour
 {
     [SerializeField] ParticleSystem particleSystem;  // Referencia al sistema de partículas
     public float newSpeed = 5f; // Nueva velocidad de las partículas
+    public Light lightSource;
+    public float maxIntensity = 5f;       // Intensidad máxima de la luz
+    public float minIntensity = 0f;       // Intensidad mínima de la luz
 
     void Start()
     {
         var main = particleSystem.main;  // Obtener el módulo Main
         main.startSpeed = newSpeed;      // Cambiar la velocidad de emisión
+    }
+    private void Update()
+    {
+        if (particleSystem.isPlaying)
+        {
+            // Aumentar intensidad basado en la cantidad de partículas activas
+            lightSource.intensity = Mathf.Lerp(lightSource.intensity, maxIntensity, Time.deltaTime * 1f);
+        }
+        else
+        {
+            // Reducir la intensidad cuando las partículas se detienen
+            lightSource.intensity = Mathf.Lerp(lightSource.intensity, minIntensity, Time.deltaTime * 1f);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
