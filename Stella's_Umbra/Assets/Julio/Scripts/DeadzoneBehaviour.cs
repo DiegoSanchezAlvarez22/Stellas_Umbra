59ff858f809aba612485a5add63613fc0e97dda8
KirasK15 <kirastheking@gmail.com>
2025-02-18 02:24:17 +0100

    Commit Deadzone implementada
    Deadzone implementada de forma correcta creo

---
+++ Stella's_Umbra/Assets/Julio/Scripts/DeadzoneBehaviour.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadzoneBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Si el jugador choca con la DeadZone:
+        if (other.CompareTag("Player"))
+        {
+            //Llamas al script de la vida y al del checkpoint
+            VidaJugador vidaJugador = other.GetComponent<VidaJugador>();
+            CheckPointSystem checkPointSystem = other.GetComponent<CheckPointSystem>();
+
+            //Comprobar si hay un dato guardado
+            if (checkPointSystem != null && PlayerPrefs.HasKey("PlayerVida"))
+            {
+                //Cargar el último checkpoint
+                checkPointSystem.LoadProgress();
+                Debug.Log("Cargando el último checkpoint...");
+            }
+            else
+            {
+                Debug.Log("No hay checkpoint guardado. Regresando al menú principal...");
+                //Carga la escena del menú principal
+                SceneManager.LoadScene("Menu Principal");
+            }
+        }
+    }
+}
